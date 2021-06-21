119e5083fd8abffc74a2c0f2e7db7a69e46c2c36
hy298 <haiyang.yun@gmail.com>
2021-06-21 22:32:00 +0200

    Delete ChIP-seq directory

--- ChIP-seq/peakCalling_H3K27ac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-GSE=$1
-GSM=$2
-pval=$3
-control=$4
-REPORT=$GSM.$pval.log #$4=pvalue
-mkdir -p $GSE/$GSM/PeakCalling/MACS.NoModel
-cd $GSE/$GSM/PeakCalling/MACS.NoModel
-./run_MACS.NoModel.pl $GSM.nodup.BED $pval $control  2> $REPORT
